6eb7a65af1adf52827087c5a65443f636e623360
Caleb <calebcsanchez1@gmail.com>
2019-08-28 11:18:25 -0400

    initializing repository

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FriendlyBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
